43cd2f594ff814c6dceaae7daa4ac9f555fa1f42
Shashikesh <uitshashikesh@gmail.com>
2023-12-24 23:56:03 +0530

    ADD s

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express')
+const dbConn = require('./config/dbConn')
+const dotenv = require('dotenv').config()
+const route = require('./routes/userRoute')
+var cors = require('cors')
+const errorHandler = require('./middleware/errorHandler')
 
-const dotenv = require('dotenv').config();
-const app = express();
+dbConn()
+const app = express()
 
+app.use(cors())
+app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/Rishi").then(() => {
-  console.log("Connnected");
-});
-const makeSchema = new mongoose.Schema({
-  name: String,
-  roll: Number,
-  branch: String,
-});
-const Student = new mongoose.model("Student", makeSchema);
+app.use('/api', route)
+app.use(errorHandler)
 
-new Student({
-  name: "shashikesh",
-  roll: 20211043,
-  branch: "CSE",
-}).save();
-
-app.get("/", (req, res) => {
-  res.send("HELLO WORLD");
-});
 app.listen(process.env.PORT, () => {
-  console.log("server is running on" ,process.env.PORT);
-});
+  console.log('Server is running on port', process.env.PORT)
+})
